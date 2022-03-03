69a54d01efadf84e1ea501d362a0a51c2c872d53
blakedoyle93 <86337789+blakedoyle93@users.noreply.github.com>
2022-03-03 20:12:39 +1100

    implement function

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,32 @@
-import { httpGet } from './mock-http-interface';
+import { httpGet } from "./mock-http-interface";
 
 // TODO define this type properly
-type TResult = any;
+type TResult = { [key: string]: string };
 
-export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
+type HttpResponse = { status: number; body: string };
+
+const RESPONSE_MAPPINGS: { [key: number]: string } = {
+  200: "Arnie Quote",
+  500: "FAILURE",
+};
+
+const parseHttpResponse = (httpResponse: HttpResponse) => ({
+  [RESPONSE_MAPPINGS[httpResponse.status]]: JSON.parse(httpResponse.body)
+    .message,
+});
+
+export const getArnieQuotes = async (urls: string[]): Promise<TResult[]> => {
   // TODO: Implement this function.
-  
-  return [];
+  const pendingPromises = [];
+
+  for (let i = 0; i < urls.length; i++) {
+    const promise = httpGet(urls[i]);
+    pendingPromises.push(promise);
+  }
+
+  const results = await Promise.all(pendingPromises).then((res) =>
+    res.map((httpResponse) => parseHttpResponse(httpResponse))
+  );
+
+  return results;
 };
